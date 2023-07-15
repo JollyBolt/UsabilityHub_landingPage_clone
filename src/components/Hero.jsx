fd5b3a691362c96c5aa0400f053bc14f6ff33084
Ishan <Braviaspan2002@gmail.com>
2023-07-16 00:55:50 +0530

    Fixed margin in Hero

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,7 +4,7 @@ const Hero = () => {
   return (
     <section className='mx-[3%] xl:mx-auto my-[35px] max-w-[1186px]'>
         <div className="flex flex-col-reverse md:flex-row items-center">
-            <div className="flex flex-col md:w-5/12 items-center md:justify-center md:items-start">
+            <div className="flex flex-col mt-6 md:mt-0 md:w-5/12 items-center md:justify-center md:items-start">
                 <p className='mb-4 text-5xl font-semibold text-center md:text-left'>Design confidently.</p>
                 <p className='text-gray-500 text-xl mb-8 text-center md:text-left'>UsabilityHub is a remote user research platform that takes the guesswork out of design decisions by validating them with real users.</p>
                 <button className='flex-initial py-3 px-6 rounded-[4px] font-semibold text-white bg-lightblue '>Get Started</button>
